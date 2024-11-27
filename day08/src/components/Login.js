c9ccbbcbe97bd71025af94af494ec5c4cf72fbda
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-11-27 23:57:25 +0530

    update login.js with validation

--- day08/src/components/Login.js
+++ day08/src/components/Login.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
 export default function login() {
-  return (
-    <div>
-      <h1>Login here</h1>
-    </div>
-  );
+    const navigate = useNavigate();
+    const users = ({
+        username:'admin',
+        password:'123'
+    });
+    const [ error, seterror ] = useState("");
+    const [ user, setuser ] = useState("");
+    
+    const authenticate = (e) => {
+        e.preventDefault();
+
+        if (!username || !password) {
+            seterror("Please provide all the fields");
+            return;
+        }
+
+        if (e.password.length < 8) {
+            seterror("Password must contain 8 charcters");
+            return;
+        }
+
+        if (users.username === e.username || users.password === e.password) {
+            navigate("/dashboard");
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login Here</h1>
+            <form onSubmit={authenticate}>
+                <input type="text" name="username" placeholder="Enter username" value={users.username} />
+                <input type="password" name="password" placeholder="Enter password" value={users.password} />
+                <button onClick={authenticate}>Click Me</button>
+            </form>
+        </div>
+    );
 }
