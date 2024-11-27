8c92d3d989c55947811f4236d828c4853569b806
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-11-27 23:43:08 +0530

    create components files for development

---
+++ day08/src/components/Dashboard.js
@@ -0,0 +1,7 @@
+export default function dashboard() {
+  return (
+    <div>
+      <h1>Welcome Admin</h1>
+    </div>
+  );
+}
---
+++ day08/src/components/Login.js
@@ -0,0 +1,7 @@
+export default function login() {
+  return (
+    <div>
+      <h1>Login here</h1>
+    </div>
+  );
+}
